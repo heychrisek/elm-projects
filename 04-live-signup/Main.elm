5407d1390e4ab3f044269850880107524d713ec4
chriskohlbrenner <c.kohlbrenner@gmail.com>
2016-03-08 18:58:31 -0500

    hit github to validate username

--- 04-live-signup/Main.elm
+++ 04-live-signup/Main.elm
@@ -1,38 +1,116 @@
 -- http://tech.noredink.com/post/129641182738/building-a-live-validated-signup-form-in-elm
 
--- declares that this is the SignupForm module, which is how
--- other modules will reference this one if they want to import it and reuse its code.
+-- run `elm make Main.elm --output signup.js` to compile signup.js, which is used in index.html
+
 module SignupForm where
-
--- Elm’s "import" keyword works mostly like "require" in node.js.
--- The “exposing (..)” option specifies that we want to bring the Html module’s contents
--- into this file’s current namespace, so that instead of writing out
--- Html.form and Html.label we can just use "form" and "label" without the "Html." prefix.
+import StartApp
+import Effects
+import Http exposing (..)
+import Task exposing (Task)
+import Json.Decode exposing (succeed)
 import Html exposing (..)
-
--- This works the same way; we also want to import the entire
--- Html.Events module into the current namespace.
 import Html.Events exposing (..)
-
--- With this import we are only bringing a few specific functions into our
--- namespace, specifically "id", "type'", "for", "value", and "class".
 import Html.Attributes exposing (id, type', for, value, class)
 
-
-view model =
+view actionDispatcher model =
     form
         [ id "signup-form" ]
         [ h1 [] [ text "Sensational Signup Form" ]
         , label [ for "username-field" ] [ text "username: " ]
-        , input [ id "username-field", type' "text", value model.username ] []
+        , input
+            [ id "username-field", type' "text"
+            , type' "text"
+            , value model.username
+            , on "input" targetValue (\str -> Signal.message actionDispatcher { actionType = "SET_USERNAME", payload = str })
+            ]
+            []
+        , div [ class "validation-error"] [text model.errors.username]
+        , div [ class "validation-error"] [text (viewUsernameErrors model)]
         , label [ for "password" ] [ text "password: " ]
-        , input [ id "password-field", type' "password", value model.password ] []
-        , div [ class "signup-button" ] [ text "Sign Up!" ]
+        , input
+            [ id "password-field"
+            , type' "password"
+            , value model.password
+            , on "input" targetValue (\str -> Signal.message actionDispatcher { actionType = "SET_PASSWORD", payload = str })
+            ]
+            []
+        , div [ class "validation-error"] [text model.errors.password]
+        , div [ class "signup-button", onClick actionDispatcher { actionType = "VALIDATE", payload = "" } ] [ text "Sign Up!" ]
         ]
 
+initialErrors =
+    { username = "", password = "", usernameTaken = False }
 
--- Take a look at this starting model we’re passing to our view function.
--- Note that in Elm syntax, we use = to separate fields from values
--- instead of : like JavaScript uses for its object literals.
+initialModel = { username = "", password = "", errors = initialErrors }
+
+viewUsernameErrors model =
+    if model.errors.usernameTaken then
+        "That username is taken!"
+    else
+        model.errors.username
+
+getErrors model =
+    { username =
+        if model.username == "" then
+            "Please enter a username!"
+        else
+            ""
+    , password =
+        if model.password == "" then
+            "Please enter a password!"
+        else
+            ""
+    , usernameTaken = model.errors.usernameTaken
+    }
+
+app =
+    StartApp.start
+        { init = ( initialModel, Effects.none )
+        , update = update
+        , view = view
+        , inputs = []
+        }
+
+update action model =
+    if action.actionType == "VALIDATE" then
+        let
+            url =
+                "https://api.github.com/users/" ++ model.username
+            usernameTakenAction =
+                { actionType = "USERNAME_TAKEN", payload = " " }
+            usernameAvailableAction =
+                { actionType = "USERNAME_AVAILABLE", payload = " " }
+            request =
+                Http.get (succeed usernameTakenAction) url
+            neverFailingRequest =
+                Task.onError request (\err -> Task.succeed usernameAvailableAction)
+        in
+            ( { model | errors = getErrors model }, Effects.task neverFailingRequest )
+    else if action.actionType == "SET_USERNAME" then
+        ( { model | username = action.payload }, Effects.none )
+    else if action.actionType == "SET_PASSWORD" then
+        ( { model | password = action.payload }, Effects.none )
+    else if action.actionType == "USERNAME_TAKEN" then
+        ( withUsernameTaken True model, Effects.none )
+    else if action.actionType == "USERNAME_AVAILABLE" then
+        ( withUsernameTaken False model, Effects.none )
+    else
+        ( model, Effects.none )
+
+withUsernameTaken isTaken model =
+    let
+        currentErrors =
+            model.errors
+
+        newErrors =
+            { currentErrors | usernameTaken = isTaken }
+    in
+        { model | errors = newErrors }
+
 main =
-    view { username = "", password = "" }
+    app.html
+
+port tasks : Signal (Task Effects.Never ())
+
+port tasks =
+    app.tasks
